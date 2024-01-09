5c9563d2ec5fe75676836c268d896a079221edd5
tunik <tunik@DESKTOP-3KGM043>
2024-01-09 23:16:16 +0500

    Добавьте файлы проекта.

---
+++ Lab2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+class main
+{
+    static void Main()
+    {
+        Console.WriteLine("Сколько цифр нужно?");
+        int n = int.Parse(Console.ReadLine());
+        if (n < 1)
+            return;
+        int[] arr1 = new int[n];
+        Console.WriteLine("Введите цифры:");
+        for (int i = 0; i < n; i++)
+        {
+            arr1[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Средний показатель: " + Task1.GetAverage(arr1));
+
+        int[] arr2 = new int[n + 1];
+        Console.WriteLine("Введите цифры:");
+        for (int i = 0; i < n; i++)
+        {
+            arr2[i] = int.Parse(Console.ReadLine());
+        }
+        arr2[n] = 0;
+        Console.WriteLine(Task2.CountQuantity(arr2));
+
+        Console.WriteLine(Task3.CountAlternation(n));
+    }
+}
+
+public class Task1
+{
+    public static double GetAverage(int[] arr)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum / arr.Length;
+    }
+}
+
+public class Task2
+{
+    public static string CountQuantity(int[] arr)
+    {
+        int negative = 0;
+        int positive = 0;
+        int i = 0;
+        while (i < arr.Length)
+        {
+            if (arr[i] % 2 != 0)
+                negative++;
+            else
+                positive++;
+            i++;
+        }
+
+        if (negative > positive)
+            return "negative";
+        if (negative < positive)
+            return "positive";
+        return "equal";
+    }
+}
+
+public class Task3
+{
+    public static int CountAlternation(int n)
+    {
+        int res = 0;
+        int i = 1;
+        do
+        {
+            if (i % 2 != 0)
+                res += i;
+            else
+                res -= i;
+            i++;
+        } while (i <= n);
+
+        return res;
+    }
+}
